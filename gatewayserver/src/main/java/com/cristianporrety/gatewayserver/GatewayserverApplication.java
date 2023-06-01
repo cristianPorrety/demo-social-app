b324f32c72edf80d2d15df7241187e0cda52c8da
cristianPorrety <cristianporretydev@gmail.com>
2023-05-31 23:58:13 -0500

    section 11 ended

--- gatewayserver/src/main/java/com/cristianporrety/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/cristianporrety/gatewayserver/GatewayserverApplication.java
@@ -10,4 +10,20 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+		
+	@Bean
+		public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		    return builder.routes()
+		        .route(p -> p
+		            .path("/porretydev/profiles/**")
+		            .filters(f -> f.rewritePath("/porretydev/profiles/(?<segment>.*)","/${segment}")
+		            				.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://PROFILES")).
+		        route(p -> p
+			            .path("/porretydev/posts/**")
+			            .filters(f -> f.rewritePath("/porretydev/posts/(?<segment>.*)","/${segment}")
+			            		.addResponseHeader("X-Response-Time",new Date().toString()))
+			            .uri("lb://POSTS")).build();
+		}
+
 }
